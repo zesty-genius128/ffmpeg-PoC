d16c8bb0ceae7c954f1938fd4cf8369ff8991791
Aditya Damerla <75409196+zesty-genius128@users.noreply.github.com>
2025-09-09 20:46:00 -0400

    nuke

--- modules/ffmpeg-merge/src/FFmpegMergeModule.ts
+++
@@ -1,7 +0,0 @@
-import { NativeModulesProxy } from 'expo-modules-core';
-
-import { FFmpegMergeModule } from './FFmpegMerge.types';
-
-// It loads the native module object from the JSI or falls back to
-// the bridge module (from NativeModulesProxy) if the remote debugger is on.
-export default NativeModulesProxy.FFmpegMerge as FFmpegMergeModule;
